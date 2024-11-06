34a79425659424c5ca9906b3b84c80948f681956
Hassan Jamshaid <hassanjamshaid364@gmail.com>
2024-11-07 00:06:33 +0500

    Client Section made

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './sections/navbar.jsx';
 import Hero from './sections/Hero.jsx';
 import About from './sections/About.jsx';
 import Projects from './sections/Projects.jsx';
+import Clients from './sections/Clients.jsx';
 const App = () => {
   return (
     <main className='max-w-7xl mx-auto'>
@@ -10,6 +11,7 @@ const App = () => {
       <Hero/>
       <About/>
       <Projects/>
+      <Clients/>
     
     </main>
   )
