5148d8d28fa21cb72eb5c4739302665f1f96c892
Hassan Jamshaid <hassanjamshaid364@gmail.com>
2024-11-07 00:58:36 +0500

    Contact page made

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './sections/Hero.jsx';
 import About from './sections/About.jsx';
 import Projects from './sections/Projects.jsx';
 import Clients from './sections/Clients.jsx';
+import Contact from './sections/Contact.jsx';
 const App = () => {
   return (
     <main className='max-w-7xl mx-auto'>
@@ -12,6 +13,8 @@ const App = () => {
       <About/>
       <Projects/>
       <Clients/>
+      
+      <Contact/>
     
     </main>
   )
