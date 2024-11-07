e38c8bd668c72a5645671dc5d32e98f0690b48a9
Hassan Jamshaid <hassanjamshaid364@gmail.com>
2024-11-07 21:52:09 +0500

    Work Expereince section made

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Projects from './sections/Projects.jsx';
 import Clients from './sections/Clients.jsx';
 import Contact from './sections/Contact.jsx';
 import Footer from './sections/Footer.jsx';
+import Experience from './sections/Experience.jsx';
 const App = () => {
   return (
     <main className='max-w-7xl mx-auto'>
@@ -14,7 +15,7 @@ const App = () => {
       <About/>
       <Projects/>
       <Clients/>
-      
+      <Experience/>
       <Contact/>
       <Footer/>
     
